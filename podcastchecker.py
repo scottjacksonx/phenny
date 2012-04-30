56afeb25dad4496cf4779a979ec0313813c0b1a1
Scott Jackson <scottjacksonx@gmail.com>
2012-05-01 05:47:58 +1000

    Should be the last commit for the podcastchecker module.

--- podcastchecker.py
+++ podcastchecker.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import time
 import threading
 
+interval = 15 * 60
+
 def setup(phenny):
 	def checkPodcasts(phenny):
 		channel = open("channel", "r").readline().strip()
@@ -23,7 +25,6 @@ def setup(phenny):
 			"""
 
 			for podcast in podcasts:
-				print "checking " + podcast["name"]
 				xmlSource = urllib2.urlopen(podcast["feed_url"]).read()
 				soup = BeautifulSoup(xmlSource)
 
@@ -31,14 +32,11 @@ def setup(phenny):
 				if latestEpisodePublishedDate != podcast["latest_episode_date"]:
 					print "new episode found"
 					podcasts[podcasts.index(podcast)]["latest_episode_date"] = latestEpisodePublishedDate
-					# say something in chat about it
 					phenny.msg(channel, "New episode of " + podcast["name"] + " available!")
-				else:
-					print "no new episode found"
 			f = open("podcasts.json", "w")
 			f.write(json.dumps(podcasts))
 			f.close()
-			time.sleep(20)
+			time.sleep(interval)
 	targs = (phenny,)
 	t = threading.Thread(target=checkPodcasts, args=targs)
 	t.start()

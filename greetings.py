2e24e7d4c5c1a2c2d4ff1eef21143938937ecf5f
Scott Jackson <scottjacksonx@gmail.com>
2012-05-01 03:04:18 +1000

    added greetings.py

---
+++ greetings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os.path, time
+from subprocess import call
+
+def greeting(phenny, input):
+	# check modification date of file 'lastupdated'
+	# if modification date > 4 hours ago, say "good evening" / "good morning" / etc.
+	try:
+		modificationDate = os.path.getmtime("lastupdated")
+	except os.error:
+		call(["touch", "lastupdated"])
+		return
+	if modificationDate < time.time() - 4*60*60:
+		phenny.say("it's been a while, " + input.nick)
+	call(["touch", "lastupdated"])

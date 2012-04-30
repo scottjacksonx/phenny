fcfe22b1be3837b21ab709102ad2dab0fd3ce699
Scott Jackson <scottjacksonx@gmail.com>
2012-05-01 03:24:53 +1000

    Should be the final(ish) update to greetings.

--- greetings.py
+++ greetings.py
@@ -5,6 +5,8 @@ from subprocess import call
 
 os.environ['TZ'] = "Australia/Brisbane"
 
+minimumTimeForHuToGreetYou = 10
+
 def greeting(phenny, input):
 	# check modification date of file 'lastupdated'
 	# if modification date > 4 hours ago, say "good evening" / "good morning" / etc.
@@ -13,10 +15,12 @@ def greeting(phenny, input):
 	except os.error:
 		call(["touch", "lastupdated"])
 		return
-	if modificationDate < time.time() - 10:
-		phenny.say("it's been a while, " + input.nick)
-	print time.ctime()
-	print time.time()
+	if modificationDate < time.time() - minimumTimeForHuToGreetYou:
+		localTime = time.localtime()
+		if 7 <= localTime.time_hour <= 11:
+			phenny.say("Good morning, " + input.nick + ". I trust you slept well.")
+		elif 18 <= localTime.time_hour:
+			phenny.say("Good evening, " + input.nick + ". I hope you had a good day.")
 	call(["touch", "lastupdated"])
 	
 greeting.rule = r'.*'

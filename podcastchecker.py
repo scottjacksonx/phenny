445276b2be3d233b7462f12442089d6bf8919d24
Scott Jackson <scottjacksonx@gmail.com>
2012-05-02 17:38:41 +1000

    Added support for checking TV shows to the podcastchecker module.

--- podcastchecker.py
+++ podcastchecker.py
@@ -13,6 +13,8 @@ def setup(phenny):
 		apikey = open("prowl_api_key", "r").readline().strip()
 		prowl = prowlpy.Prowl(apikey)
 		while True:
+			
+			# check podcasts
 			f = open("podcasts.json", "r")
 			podcasts = json.loads(f.read())
 			f.close()
@@ -39,6 +41,35 @@ def setup(phenny):
 					prowl.post(application="hu",event="Podcast",description="New episode of " + podcast["name"] + " available!")
 			f = open("podcasts.json", "w")
 			f.write(json.dumps(podcasts))
+			f.close()
+			
+			# check TV shows
+			f = open("tv.json", "r")
+			shows = json.loads(f.read())
+			f.close()
+
+			"""
+			a tv show looks like this:
+
+			{
+				feed_url: "http://something"
+				latest_episode_date: "something"
+				name: "The Name of the Show"
+			}
+			"""
+
+			for show in shows:
+				xmlSource = urllib2.urlopen(show["feed_url"]).read()
+				soup = BeautifulSoup(xmlSource)
+
+				latestEpisodePublishedDate = soup.item.pubdate.string
+				if latestEpisodePublishedDate != show["latest_episode_date"]:
+					print "new episode found"
+					shows[shows.index(show)]["latest_episode_date"] = latestEpisodePublishedDate
+					phenny.msg(channel, "New episode of " + show["name"] + " available!")
+					prowl.post(application="hu",event="TV Show",description="New episode of " + show["name"] + " available!")
+			f = open("tv.json", "w")
+			f.write(json.dumps(shows))
 			f.close()
 			time.sleep(interval)
 	targs = (phenny,)
